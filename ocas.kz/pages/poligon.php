1d4ee4e6e43ff033dd19d365631a5db5a7a0455a
MagzhanOkas <magzhanokas07@gmail.com>
2023-11-24 00:48:58 +0600

    New again

---
+++ ocas.kz/pages/poligon.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Полигон</title>
+</head>
+<body>
+Здесь находятся мои творческие, тестируемые, экспериментальные работы c помощью CSS + JS
+</body>
+</html>
